a139b08174f6a1db7102865f587f6751a5696f7a
Rafał Chrebela <rafal.chrebela@gmail.com>
2018-02-14 18:05:45 +0100

    Add code to scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,14 @@
 // scripts.js
+var a = 1;
+var b = 3;
+
+var value = (a * a) - (2 * a * b) - (b * b);
+console.log('Wynik wynosi:' + value)
+
+if (value < 0) {
+  console.log('Wynik ujemny')
+} if (value >= 1) {
+  console.log('Wynik dodatni')
+} else if (value != 0 ) {
+  console.log('Wynik nie jest równy zero')
+} 
